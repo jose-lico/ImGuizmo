dbc93536596136e18ab72a6bfc6a054cc32172a5
José Lico <57019940+jose-lico@users.noreply.github.com>
2022-11-30 18:58:22 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,39 +1,40 @@
 project "ImGuizmo"
 	kind "StaticLib"
 	language "C++"
-	configmap {
-        ["ReleaseNoImGui"] = "Release"
-    }
+	configmap 
+	{
+		["ReleaseWithInfo"] = "Release",
+		["ReleaseNoImGui"] = "Release"
+	}
 
 	targetdir ("%{wks.location}/bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputDir .. "/%{prj.name}")
 
-    files
+	files
 	{
-        "ImGuizmo.cpp",
-	    "include/ImGuizmo/ImGuizmo.h"
+		"ImGuizmo.cpp",
+		"include/ImGuizmo/ImGuizmo.h"
 	}
 
-    includedirs
+	includedirs
 	{
-        "include/ImGuizmo",
-        dependenciesDir .. "/imgui/include/imgui"
-    }
+		"include/ImGuizmo",
+		dependenciesDir .. "/imgui/include/imgui"
+	}
 
-    filter "system:windows"
+	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "on"
 		systemversion "latest"
 
-    filter "system:linux"
+	filter "system:linux"
 		cppdialect "C++17"
 		systemversion "latest"
 
-    filter "configurations:Debug"
+	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "On"
-	
+
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "On"
-		
